d549b0f58f99948323e26fc6cf456c8c62078a80
DESKTOP-C6M0LJK\Jenesius <lokargenia@gmail.com>
2018-10-23 02:49:29 +0300

    Добавление функции,которая реагирует на добавление кнопок и графика

--- inject.js
+++ inject.js
@@ -1,6 +1,27 @@
 'use strict';
 
+var HeightBlock = $(".discussions__list").height();
+var buttonComm = '<button class = "ButtonComments">test</button>';
+function addDate()
+{
+    HeightBlock = $(".discussions__list").height();
+    var children=$('.discussions__list').children();
+    $('.discussions__list').children().each(function () {
+        if($(this).find('.ButtonComments' ).length == 0)
+        {
+            $(this).find( ".comment-widget__header" ).after(buttonComm);//insert comments
+                                                                        //insert canvas
+        }
+    });
+}
 
+
+$(window).scroll(function () {
+    if (HeightBlock != $(".discussions__list").height())
+        addDate();
+});
+
+/*
 document.addEventListener('click', function () {
     let commentHeader = document.getElementsByClassName("comment-widget__header");
 
@@ -20,5 +41,5 @@ document.addEventListener('click', function () {
             getAccessToken(0);
         });
     });
-});
-// <button class="discussions__load-btn small white expand" data-ember-action="1607">Показать обсуждения (1)</button>
+});*/
+// <button class="discussions__load-btn small white expand" data-ember-action="1607">Показать обсуждения (1)</button>ton class="discussions__load-btn small white expand" data-ember-action="1607">Показать обсуждения (1)</button>

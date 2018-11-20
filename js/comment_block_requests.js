ee0e46c41138cc0c03a35591f90a099d2370d4b5
DESKTOP-QSDEP9N\nikva <nik.vag@mail.ru>
2018-11-20 17:40:34 +0300

    Merge

--- js/comment_block_requests.js
+++ js/comment_block_requests.js
@@ -1,6 +1,4 @@
-// import {getAccessToken,getSectionId,getStepId,getSolutionId,getCourseId,getUserCost,getUserScore} from "./requests";
-
-  function getLastSolutionURL(user_id){
+ function getLastSolutionURL(user_id){
     let solution_id = getSolutionId(STEP_ID, user_id, ACCESS_TOKEN);
     return base_url + "/submissions/" + STEP_ID + "/" + solution_id;
 
--- js/service_requests.js
+++ js/service_requests.js
@@ -3,12 +3,13 @@ var ACCESS_TOKEN, STEP_ID, SOLUTION_LIST, SECTION_ID, COURSE_ID, USER_COST;
 function getServiceInfo(lesson_id, step_index) {
     return new Promise(function(resolve, reject)
     {
-        ACCESS_TOKEN = getAccessToken();
-        STEP_ID = getStepId(lesson_id, step_index, ACCESS_TOKEN);
-        SOLUTION_LIST = getSolutionList(STEP_ID, ACCESS_TOKEN);
-        SECTION_ID = getSectionId(lesson_id, ACCESS_TOKEN);
-        COURSE_ID = getCourseId(SECTION_ID, ACCESS_TOKEN);
-        USER_COST = getUserCost(COURSE_ID,ACCESS_TOKEN);
-        return resolve(1);
+        if (ACCESS_TOKEN = getAccessToken() !== undefined)
+            if (STEP_ID = getStepId(lesson_id, step_index, ACCESS_TOKEN) !== undefined)
+                if (SOLUTION_LIST = getSolutionList(STEP_ID, ACCESS_TOKEN) !== undefined)
+                    if (SECTION_ID = getSectionId(lesson_id, ACCESS_TOKEN) !== undefined)
+                        if (COURSE_ID = getCourseId(SECTION_ID, ACCESS_TOKEN) !== undefined)
+                            if (USER_COST = getUserCost(COURSE_ID,ACCESS_TOKEN) !== undefined)
+                                return resolve(1);
+        return reject(100);
     });
 }
